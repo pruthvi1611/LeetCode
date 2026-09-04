52bdad9df9dded0a7a22fc858ceb816fafc35e43
pruthvi1611 <pruthvipatel889@gmail.com>
2026-09-04 23:11:56 +0530

    Leetcode

---
+++ 42_trapping_rainwater.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int leftMax[20000];
+        int rightMax[20000];
+        int n = height.size();
+        leftMax[0] = height[0];
+        rightMax[n-1] = height[n-1];
+        for(int i=1;i<n;i++){
+            leftMax[i] = max(leftMax[i-1],height[i-1]);
+        }
+        for(int i=n-2;i>=0;i--){
+            rightMax[i] = max(rightMax[i+1],height[i+1]);
+        }
+        int waterTrapped = 0 ;
+        for(int i=0;i<n;i++){
+            int currWater = min(leftMax[i],rightMax[i]) - height[i];
+
+            if(currWater > 0){
+                waterTrapped +=  currWater;
+            }
+        }
+        return waterTrapped;
+    }
+};
+
+int main(){
+    vector<int> trap= {1,3,1,3,6,7,2};
+    Solution tra;
+    cout << "Water Trapped : " << tra.trap(trap);
+}
